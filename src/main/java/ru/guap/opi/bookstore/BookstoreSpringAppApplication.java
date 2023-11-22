44f13ea326f365adee60c003f63bb06669d98d4b
SirPersimmon <atafeeva@icloud.com>
2023-11-22 15:15:20 +0300

    Initial commit

---
+++ src/main/java/ru/guap/opi/bookstore/BookstoreSpringAppApplication.java
@@ -0,0 +1,13 @@
+package ru.guap.opi.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreSpringAppApplication.class, args);
+    }
+
+}
